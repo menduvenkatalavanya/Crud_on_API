8f8b207c5d97bbdc2d35a9bddeeaaaf6bb6e0cc9
menduvenkatalavanya <lavanyamendu17@gmail.com>
2024-04-08 16:23:07 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# Crud_on_API
+Crud operations using RestAPI
+=======
+# practical
+>>>>>>> 80cfba5 (First commit)
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->save();
+
+        return response()->json(['message' => 'Product created', 'product' => $product], 201);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        $data = [
+            'status' =>200,
+            'products' =>$products
+        ]; 
+
+        return response()->json(['products' => $data], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        return response()->json(['product' => $product], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->save();
+
+        return response()->json(['message' => 'Product updated', 'product' => $product], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted'], 200);
+    }
+}
